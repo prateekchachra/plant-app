b045731a513d8549ae5875487cb620c6164d8ca0
Prateek Chachra <prateekchachra@live.com>
2019-06-28 00:07:36 +0530

    Initialized the project

---
+++ navigation/index.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import {Image} from 'react-native';
+import {createAppContainer, createStackNavigator} from 'react-navigation';
+
+
+import Welcome from '../screens/Welcome';
+// import Login from '../screens/Login';
+// import Explore from '../screens/Explore';
+// import Browse from '../screens/Browse';
+// import Product from '../screens/Product';
+// import Settings from '../screens/Settings';
+
+
+
+import {theme} from '../constants';
+import { nullLiteral } from '@babel/types';
+
+
+const screens = createStackNavigator({
+    Welcome,
+// Login,
+// Explore,
+// Browse,
+// Product,
+// Settings,
+},
+{defaultNavigationOptions:{
+    headerStyle: {},
+    headerBackImage: <Image />,
+    headerBackTitle: nullLiteral,
+    headerLeftContainerStyle: {},
+    headerRightContainerStyle: {}
+
+}
+});
+
+
+export default createAppContainer(screens);
